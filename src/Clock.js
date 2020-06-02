ef27406fc7f3f810e3ae054d3671c073b50c408b
almazu <almazuleyma@gmail.com>
2020-06-01 18:40:34 -0700

    Update: Clock shows ticking time

--- src/Clock.js
+++ src/Clock.js
@@ -9,11 +9,11 @@ class Clock extends React.Component {
     render() {
         return (
             <div className="clock">
-                <h2>Art History Clock</h2>
-                <h4>Quelle heure est-il?</h4>
+                <h4>Art History Clock</h4>
+                <h3>Quelle heure est-il?</h3>
                 <Time />
                 <Painting />
-                <h4>Set Alarm:</h4>
+                <h5>Set Alarm:</h5>
                 <Alarm />
                 <footer>Developed by Alma Zuleyma 2020</footer>
             </div>
@@ -22,10 +22,31 @@ class Clock extends React.Component {
 }
 
 class Time extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: (new Date().toLocaleTimeString())
+        }
+
+    }    
+    componentDidMount() {
+        this.intervalID = setInterval(
+            () => this.tick(),
+            1000
+        );        
+    }
+    componentWillUnmount() {
+        clearInterval(this.interalID);
+    }
+    tick() {
+        this.setState({
+            time: new Date().toLocaleTimeString()
+        });
+    }
+
     render() {
         return (
-
-            <div><h1>{new Date().toLocaleTimeString()}</h1></div>
+            <div><h1>{this.state.time}</h1></div>
         )
     }
 }
@@ -40,7 +61,7 @@ class Painting extends React.Component {
         };
     }
 
-    async componentDidMount() {
+    componentDidMount() {
         const url = "./artwork.json"
         fetch(url)
             .then(res => {
@@ -60,11 +81,11 @@ class Painting extends React.Component {
         } else {
             return (
                 <div className="painting">
-                    <img className="image" src={artwork[getTime()].path} alt="Artwork"></img>
+                    <img src={artwork[getTime()].path} alt="Artwork"></img>
                     <span className="description">
-                        <p>{artwork[getTime()].title}, {artwork[getTime()].year} </p>
-                        <p>{artwork[getTime()].artist}</p>
-                        <p><a href={artwork[getTime()].link}>Artwork info</a></p>
+                        <span className="title-italic">{artwork[getTime()].title}</span>, {artwork[getTime()].year} <br />
+                        {artwork[getTime()].artist} <br />
+                        <a href={artwork[getTime()].link}>Artwork info</a>
                     </span>
                 </div>
             )
@@ -111,7 +132,6 @@ class Alarm extends React.Component {
         )
     }
 }
-
 
 function getTime() {
     return (
